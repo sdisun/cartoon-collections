cdf77df89e5c64067ab52139c3b3c4c7f5e77aeb
Neil <zooming.in@hotmail.com>
2021-07-09 00:23:03 +1000

    updates

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (array)
+  array.collect do |dwarf|
+    "#{dwarf.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |element|
+    element.size > 4 ? true : false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |snack|
+    if cheese_types.include?(snack)
+      return snack
+    end
+  end
+
 end
